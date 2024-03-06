84ad1d84c18879e358841597a1ec24339b484a4a
Michelle Dadighat <michelledadighat@gmail.com>
2024-03-05 16:12:14 -0800

    Revert "Email confirmation, error reporting, clean up"

--- repository/admin.py
+++ repository/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Location, Observation, Satellite
+from .models import Image, Location, Observation, Satellite
 
 admin.site.register(Satellite)
 admin.site.register(Location)
+admin.site.register(Image)
 admin.site.register(Observation)
--- repository/urls.py
+++ repository/urls.py
@@ -1,18 +1,12 @@
-from django.conf import settings
-from django.urls import include, path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("data-format", views.data_format, name="data-format"),
-    path("view", views.view_data, name="view-data"),
+    path("data-format.html", views.data_format, name="data-format"),
+    path("view.html", views.view_data, name="view-data"),
     path("download-all", views.download_all, name="download-all"),
     path("download-results", views.download_results, name="download-results"),
-    path("search", views.search, name="search"),
-    path("upload", views.upload, name="upload-obs"),
-    path("download-ids", views.download_obs_ids, name="download-obs-ids"),
-    path(r"ht/" + settings.SECRET_HEALTH_CHECK_TOKEN, include("health_check.urls")),
-    path("about", views.about, name="about"),
-    path("download", views.download_data, name="download-data"),
+    path("search.html", views.search, name="search"),
 ]
