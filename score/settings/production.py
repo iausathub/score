06525b30e871636960eb577409e4a6fa603e1f82
Michelle Dadighat <michelle.dadighat@noirlab.edu>
2024-06-19 10:13:26 -0700

    Format change to csrf trusted origins setting

--- score/settings/production.py
+++ score/settings/production.py
@@ -19,10 +19,10 @@ ALLOWED_HOSTS.append(gethostbyname(gethostname()))
 
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-CSRF_TRUSTED_ORIGINS = get_secret("score-allowed-hosts")[
-    "score-prod-alb-csrf"
+CSRF_TRUSTED_ORIGINS = [
+    get_secret("score-allowed-hosts")["score-prod-alb-csrf"]
 ]  # noqa: F405
-
+print(CSRF_TRUSTED_ORIGINS)
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
--- score/settings/test.py
+++ score/settings/test.py
@@ -19,8 +19,8 @@ ALLOWED_HOSTS.append(gethostbyname(gethostname()))
 
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-CSRF_TRUSTED_ORIGINS = get_secret("score-allowed-hosts")[
-    "score-prod-alb-csrf"
+CSRF_TRUSTED_ORIGINS = [
+    get_secret("score-allowed-hosts")["score-prod-alb-csrf"]
 ]  # noqa: F405
 print(CSRF_TRUSTED_ORIGINS)
 
