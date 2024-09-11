8cc4611afbe73775efa158dd20ad9eb497e4463c
Michelle Dadighat <michelle.dadighat@noirlab.edu>
2024-09-11 15:36:44 -0700

    Add whitenoise

--- score/settings/base.py
+++ score/settings/base.py
@@ -119,6 +119,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "score.urls"
