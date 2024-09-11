76044f7d4ab7647a28643b3a5404ca00e2242e70
Michelle Dadighat <michelle.dadighat@noirlab.edu>
2024-09-10 17:20:37 -0700

    Test static files path change

--- score/settings/base.py
+++ score/settings/base.py
@@ -183,9 +183,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = "static"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_FINDERS = [
     "compressor.finders.CompressorFinder",
