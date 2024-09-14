0f9288483b6a57ebde501d4ab8f805f44c639adf
Jason Kalawe <jason.kalawe@noirlab.edu>
2024-09-13 17:59:49 -1000

    remove kubernetes script and moved contents to

--- dev/kubernetes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Copy Django generated static files to shared files
-# NGINX will serve these files as it also mounts the shared files volume
-cp -r /usr/src/app/static /shared-files/
--- start.sh
+++ start.sh
@@ -6,6 +6,10 @@ python manage.py migrate
 echo "${0}: collecting static files."
 python manage.py collectstatic --noinput
 
+# Copy Django generated static files to shared files
+# NGINX will serve these files as it also mounts the shared files volume
+cp -r /usr/src/app/static /shared-files/
+
 echo "${0}: starting server."
 ENV DJANGO_SETTINGS_MODULE=$1
 gunicorn -b 0.0.0.0 -w 2 score.wsgi:application
